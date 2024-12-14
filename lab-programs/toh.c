4a5544f83c71f5080033392cc2960d21ebb97115
bakeyed <yuverseat@gmail.com>
2024-12-15 01:57:29 +0530

    hope this works:(

---
+++ lab-programs/toh.c
@@ -0,0 +1,19 @@
+//Program to solve Tower of Hanoi using recurssion
+#include <stdio.h>
+#include<stdlib.h>
+void TOH (int n, char src, char dest, char temp) {
+    if (n>0) {
+        TOH(n-1, src, temp, dest);
+        printf("Move disk %d from %c to %c\n", n,src, dest);
+        TOH(n-2, temp, dest, src);
+    }
+    return;
+}
+int main() {
+    int n;
+    printf("E\n");
+    printf("Enter disk number: ");
+    scanf("%d", &n);
+    TOH(n, 'A', 'C', 'B');
+    return 0;
+}
